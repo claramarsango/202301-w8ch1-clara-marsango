5bed306e661f9f6a2d673bcefffef12126b5bae7
claramarsango <claraxmarsango@gmail.com>
2023-03-06 22:51:55 +0100

    add footer to main layout

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import styles from './MainLayout.module.css';
 
 const MainLayout = () => {
   return (
     <>
       <Outlet />
+      <footer className={styles.footer} data-testid="footer">
+        <p>Made by Clari</p>
+      </footer>
     </>
   );
 };
