f65e6c148862269bbb900fdd9224bf91f2f792cc
claramarsango <claraxmarsango@gmail.com>
2023-03-06 21:26:22 +0100

    remove character list component from home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
-import CharacterList from '../../features/characters/CharacterList/CharacterList';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <>
       <h1 className={styles.title}>Star Wars</h1>
-      <CharacterList />
     </>
   );
 };
